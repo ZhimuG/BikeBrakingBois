cb850629e4e73696dc12402e7b659b07b6b52d12
Zhimu Guo <15zg11@queensu.ca>
2019-11-04 10:54:48 -0500

    Update MPU6050_raw.ino

--- Reference Code/MPU6050_raw/MPU6050_raw.ino
+++ Reference Code/MPU6050_raw/MPU6050_raw.ino
@@ -17,7 +17,8 @@
 
 #include "MPU6050.h"
 
-MPU6050 imu;
+MPU6050 imu1;
+MPU6050 imu2;
 
 void setup()
 {
@@ -25,11 +26,20 @@ void setup()
 
     Wire.begin();
  
-    if (!imu.begin(AFS_2G, GFS_250DPS)) {
-        Serial.println("MPU6050 is online...");
+    if (!imu1.begin(AFS_2G, GFS_250DPS)) {
+        Serial.println("MPU6050 1 is online...");
     }
     else {
-        Serial.println("Failed to init MPU6050");
+        Serial.println("Failed to init MPU6050 1");
+        while (true) 
+            ;
+    }
+
+    if (!imu2.begin(AFS_2G, GFS_250DPS)) {
+        Serial.println("MPU6050 2 is online...");
+    }
+    else {
+        Serial.println("Failed to init MPU6050 2");
         while (true) 
             ;
     }
@@ -37,20 +47,38 @@ void setup()
 
 void loop()
 {  
-    int16_t ax, ay, az, gx, gy, gz;
+    int16_t ax1, ay1, az1, gx1, gy1, gz1;
+    int16_t ax2, ay2, az2, gx2, gy2, gz2;
 
-    if (imu.getMotion6Counts(&ax, &ay, &az, &gx, &gy, &gz)) {
-        Serial.print(ax);
+    if (imu1.getMotion6Counts(&ax1, &ay1, &az1, &gx1, &gy1, &gz1)) {
+        Serial.print("Device 1:\n");
+        Serial.print(ax1);
         Serial.print(" ");
-        Serial.print(ay);
+        Serial.print(ay1);
         Serial.print(" ");
-        Serial.print(az);
+        Serial.print(az1);
         Serial.print(" ");
-        Serial.print(gx);
+        Serial.print(gx1);
         Serial.print(" ");
-        Serial.print(gy);
+        Serial.print(gy1);
         Serial.print(" ");
-        Serial.print(gz);
+        Serial.print(gz1);
+        Serial.println();
+    }
+    
+    if (imu2.getMotion6Counts(&ax2, &ay2, &az2, &gx2, &gy2, &gz2)) {
+        Serial.print("Device 2:\n");
+        Serial.print(ax2);
+        Serial.print(" ");
+        Serial.print(ay2);
+        Serial.print(" ");
+        Serial.print(az2);
+        Serial.print(" ");
+        Serial.print(gx2);
+        Serial.print(" ");
+        Serial.print(gy2);
+        Serial.print(" ");
+        Serial.print(gz2);
         Serial.println();
     }
 }
