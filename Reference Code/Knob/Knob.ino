042f8c56c27b93311f0cd22500833ed00ef76e39
Zhimu Guo <15zg11@queensu.ca>
2019-11-13 22:38:23 -0500

    Update Knob.ino

--- Reference Code/Knob/Knob.ino
+++ Reference Code/Knob/Knob.ino
@@ -15,36 +15,49 @@
 
 #include <PWMServo.h>
 
-PWMServo myservo;  // create servo object to control a servo
+PWMServo myservoF;  // create servo object to control a servo
+PWMServo myservoB;
 
-const int potpin = A0;  // analog pin used to connect the potentiometer
+const int potpin = A3;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
 void setup() {
-  myservo.attach(SERVO_PIN_A);         // attaches the servo on pin 9 to the servo object
+  myservoF.attach(2);         // attaches the servo on pin 9 to the servo object
+  myservoB.attach(3);
   //myservo.attach(SERVO_PIN_A, 1000, 2000); // some motors need min/max setting
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+//  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+//  Serial.println(val);
+//  val = map(val, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180)
+  int i = 0;
+  while(1){
+  if(i<170){
+    i+=10;
+    myservoF.write(i);                  // sets the servo position according to the scaled value
+    myservoB.write(i);
+  }else{
+    i = 1;
+    myservoF.write(i);                  // sets the servo position according to the scaled value
+    myservoB.write(i);
+  }
+  delay(15);}                           // waits for the servo to get there
 }
 
 // Modular code starts here:
 // Get potentiometer input
-int getPotentiometerInput(const int potpin){
-  int val = 0;
-  val = analogRead(potpin);
-  return val;
-}
-
-// Write PWM signal to servomotor
-void writeToMotor(int* forcePWM, int* servoPorts){
-  PWMServo myservoF, myservoB;
-  myservoF.attach(servoPorts[0]);
-  myservoB.attach(servoPorts[1]);
-  myservoF.write(forcePWM[0]);
-  myservoB.write(forcePWM[1]);
-}
+//int getPotentiometerInput(const int potpin){
+//  int val = 0;
+//  val = analogRead(potpin);
+//  return val;
+//}
+//
+//// Write PWM signal to servomotor
+//void writeToMotor(int* forcePWM, int* servoPorts){
+//  PWMServo myservoF, myservoB;
+//  myservoF.attach(servoPorts[0]);
+//  myservoB.attach(servoPorts[1]);
+//  myservoF.write(forcePWM[0]);
+//  myservoB.write(forcePWM[1]);
+//}
