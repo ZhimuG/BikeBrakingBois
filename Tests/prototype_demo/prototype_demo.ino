edadd6e1edfb4422d67123aef27deaa1de5629de
Zhimu Guo <15zg11@queensu.ca>
2019-11-22 15:37:42 -0500

    Update prototype_demo.ino

--- Tests/prototype_demo/prototype_demo.ino
+++ Tests/prototype_demo/prototype_demo.ino
@@ -3,12 +3,11 @@
 // Adapted by OLBABS team
 
 #include <PWMServo.h>
+#include "SdFat.h"
+#include "BufferedPrint.h"
 
 PWMServo myservoF;  // create servo object to control a servo
 PWMServo myservoB;  // create servo object to control a servo
-
-#include "SdFat.h"
-#include "BufferedPrint.h"
 
 // For SD write:
 // SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
@@ -195,7 +194,10 @@ void loop() {
   PWM[1] = val;
   MoveMotors(PWM);
   if(val<170){
+    make_buzz(14,500);
     writeSD_move_motors(map(val, 10, 170, 0, 20), PWM);
+    delay(2000);
+    buzz_stop(14);
   }
   // sets the servo position according to the scaled value
   delay(20);                           // waits for the servo to get there
