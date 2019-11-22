8eb85b0bfbfa56155a4bdd6cb0c98cc584a46ed8
Zhimu Guo <15zg11@queensu.ca>
2019-11-22 15:49:38 -0500

    Create ReadPhotodiode.ino

---
+++ Tests/ReadPhotodiode/ReadPhotodiode.ino
@@ -0,0 +1,18 @@
+#include <PWMServo.h>
+
+PWMServo myservoF;  // create servo object to control a servo
+PWMServo myservoB;  // create servo object to control a servo
+void setup() {
+  // put your setup code here, to run once:
+    myservoB.attach(3);         // attaches the servo on pin 9 to the servo object
+  myservoF.attach(2);         // attaches the servo on pin 9 to the servo object
+  //  Serial.begin(9600);
+  myservoB.write(10);
+  myservoF.write(170);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(31));
+  delayMicroseconds(100);
+}
